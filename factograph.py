32036250c5e7969583124942743b9e919c4b5deb
leocamelo <leonardocamelo.nave@gmail.com>
2019-08-27 11:53:28 -0300

    generates only matches over product

--- factograph.py
+++ factograph.py
@@ -1,5 +1,4 @@
-from json import load
-from itertools import product
+import json
 
 from PIL import Image
 
@@ -8,6 +7,27 @@ image_w = 940
 image_h = 492
 
 cells_cache = {}
+
+
+def mount_colors(apps_file):
+    colors = {}
+
+    with open(apps_file) as f:
+        for app in json.load(f):
+            colors[app['app_id']] = app['color']
+
+    return colors
+
+
+def mount_matches(templates_file):
+    matches = set()
+
+    with open(templates_file) as f:
+        for template in json.load(f):
+            if 'coming_soon' not in template or not template['coming_soon']:
+                matches.add((template['tool_source'], template['tool_target']))
+
+    return matches
 
 
 def create_cell(app, color, w, h):
@@ -27,25 +47,24 @@ def create_cell(app, color, w, h):
 
 
 def main():
-    with open('data.json') as f:
-        data = load(f).items()
+    colors = mount_colors('apps.json')
+    matches = mount_matches('templates.json')
 
-        cell_w = image_w // 2
-        cell_h = image_h
+    cell_w = image_w // 2
+    cell_h = image_h
 
-        for (app1, color1), (app2, color2) in product(data, data):
-            if app1 != app2:
-                title = '{}-{}'.format(app1, app2)
-                image = Image.new('RGBA', (image_w, image_h))
+    for app1, app2 in matches:
+        title = '{}-{}'.format(app1, app2)
+        image = Image.new('RGBA', (image_w, image_h))
 
-                cell1 = create_cell(app1, color1, cell_w, cell_h)
-                cell2 = create_cell(app2, color2, cell_w, cell_h)
+        cell1 = create_cell(app1, colors[app1], cell_w, cell_h)
+        cell2 = create_cell(app2, colors[app2], cell_w, cell_h)
 
-                image.paste(cell1)
-                image.paste(cell2, (cell_w, 0))
+        image.paste(cell1)
+        image.paste(cell2, (cell_w, 0))
 
-                image.save('dist/{}.png'.format(title), 'PNG')
-                print(title)
+        image.save('dist/{}.png'.format(title), 'PNG')
+        print(title)
 
 
 if __name__ == '__main__':
